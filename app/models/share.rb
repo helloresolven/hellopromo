40e7e1ea2047ceac48b929c7d9373858ed80c0b5
Patrick <helloresolven@gmail.com>
2012-04-27 12:58:53 -0400

    use IS NOT NULL

--- app/models/share.rb
+++ app/models/share.rb
@@ -23,7 +23,7 @@ class Share < ActiveRecord::Base
   end
   
   def redeemed_codes
-    Redeemable.where("share_id = ? AND redeemer_id <> ?", self, nil)
+    Redeemable.where("share_id = ? AND redeemer_id IS NOT NULL", self)
   end
   
   def to_tweet
